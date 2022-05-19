6d32352dc78dc4138d761b378dcf896fb7a08ac0
Crowdbotics <team@crowdbotics.com>
2022-05-19 12:14:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nft_main_35285",
+  url: "https://nft_main_35285.botics.co",
+  api: "https://nft_main_35285.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
